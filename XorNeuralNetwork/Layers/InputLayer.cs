9d9d4514c143ae06df727efaea4ed3a096c8ffed
Dmytro Piskun <Dmytro.Piskun@caspio.com>
2018-11-23 19:33:16 +0200

    Xor added

---
+++ XorNeuralNetwork/Layers/InputLayer.cs
@@ -0,0 +1,13 @@
+﻿namespace NeuralNetwork.Layers
+{
+	internal class InputLayer
+	{
+		public (double[], double[])[] TrainSet { get; } =
+		{
+			(new double[] {0, 0}, new double[] {0, 1}),
+			(new double[] {0, 1}, new double[] {1, 0}),
+			(new double[] {1, 0}, new double[] {1, 0}),
+			(new double[] {1, 1}, new double[] {0, 1})
+		};
+	}
+}
