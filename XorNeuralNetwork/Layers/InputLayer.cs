e22f75a90801f0b658e5453c68e3d8887689ff70
dip <dmytro.piskun@gmail.com>
2018-11-26 16:41:31 +0200

    Change init

--- XorNeuralNetwork/Layers/InputLayer.cs
+++ XorNeuralNetwork/Layers/InputLayer.cs
@@ -8,13 +8,11 @@ namespace NeuralNetwork.Layers
 
         public InputLayer()
         {
-            TrainSet = new Dictionary<double[], double[]>
-            {
-                { new double[] { 0, 0 }, new double[] { 0, 1 } },
-                { new double[] { 0, 1 }, new double[] { 1, 0 } },
-                { new double[] { 1, 0 }, new double[] { 1, 0 } },
-                { new double[] { 1, 1 }, new double[] { 0, 1 } }
-            };
+            TrainSet = new Dictionary<double[], double[]>();
+            TrainSet.Add( new double[] { 0, 0 }, new double[] { 0, 1 } );
+            TrainSet.Add(new double[] { 0, 1 }, new double[] { 1, 0 });
+            TrainSet.Add(new double[] { 1, 0 }, new double[] { 1, 0 });
+            TrainSet.Add(new double[] { 1, 1 }, new double[] { 0, 1 });
         }
 	}
 }
