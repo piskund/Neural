67dc6916be96ce90c2067d9640900b76d9e109f3
dip <dmytro.piskun@gmail.com>
2018-11-27 22:29:50 +0200

    Add new project

---
+++ NeuralNetworkForms/Form1.cs
@@ -0,0 +1,28 @@
+﻿using NeuralNetwork;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuralNetworkForms
+{
+    public partial class Form1 : Form
+    {
+        Network _net= new Network();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _net.Train();
+        }
+    }
+}
